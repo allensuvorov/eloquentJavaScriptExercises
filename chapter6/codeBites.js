7cb7611a09495410579d60af00807cd0a1c1bb19
Allen Suvorov <56049635+allensuvorov@users.noreply.github.com>
2020-11-02 14:25:51 +0300

    Update codeBites.js

--- chapter6/codeBites.js
+++ chapter6/codeBites.js
@@ -1,8 +1,9 @@
+// The chapter is very hm... interesting, need to review again.
+// Format: Section Name + Sample Code
+
 // cat says meow
 let cat = {};
 cat.speaks = function() {
     console.log(`Cat says Meow`);
 };
 cat.speaks();
-
-// the chapter is very interesting, need to review again.
