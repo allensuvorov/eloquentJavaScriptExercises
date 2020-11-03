4c6b451d501af1f179283cb2ed286df45381c1b2
Allen <allenvs@mail.ru>
2020-11-03 21:59:37 +0300

    added Encapsulation notes to chapter 6 sections tracking

--- chapter6/codeBites.js
+++ chapter6/codeBites.js
@@ -1,6 +1,15 @@
 // The chapter is very hm... interesting, need to review again.
 // Format: Section Name + Sample Code
 
+// 6.1. Encapsulation
+// interfaces, public, private
+// It is also common to put an underscore (_) 
+// character at the start of property names to 
+// indicate that those properties are private.
+
+// code here
+
+// 6.2. Methods
 // cat says meow
 let cat = {};
 cat.speaks = function() {
