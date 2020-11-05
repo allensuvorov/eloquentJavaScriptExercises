14147a11c616a524181ac396ec1962431293b081
Allen <allenvs@mail.ru>
2020-11-05 23:37:47 +0300

    add inheritance example with subclass Knight

--- chapter6/Unit.js
+++ chapter6/Unit.js
@@ -2,7 +2,6 @@
 // classes, encapsulation, constructor, getters and setters, instanseof
 
 class Unit {
-
     _attack = 5;
     _health = 10;
   
@@ -13,7 +12,6 @@ class Unit {
     get name() {
       return this._name;
     }
-  
     set name (name) {
       this._name = name;
     }
@@ -21,11 +19,13 @@ class Unit {
     get health() {
       return this._health;
     }
-  
     set health (health) {
       this._health = health;
     }
    
+    get attack () {
+      return this._attack;
+    }
     set attack (attack) {
       if (attack < 5) {
         throw new Error ("Attack must be 5 of more");
@@ -33,14 +33,9 @@ class Unit {
       this._attack = attack;
     }
   
-    get attack () {
-      return this._attack;
-    }
-  
     isAlive() {
       return this._health > 0;
     }
-  
   
     attackOther(otherUnit) {
       if (!(otherUnit instanceof Unit)) {
@@ -65,15 +60,43 @@ class Unit {
   
   let tankUnit1 = new Unit("Ben");
   
-  console.log(tankUnit1._name); // not allowed
-  console.log(tankUnit1._health); // not allowed
-  console.log(tankUnit1);
-  console.log(tankUnit1.getHealth); // allowed
-  tankUnit1.health = 15;
-  console.log(tankUnit1.getHealth); // allowed
+  // console.log(tankUnit1._name); // not allowed
+  // console.log(tankUnit1._health); // not allowed
+  // console.log(tankUnit1);
+  // console.log(tankUnit1.getHealth); // allowed
+  // tankUnit1.health = 15;
+  // console.log(tankUnit1.getHealth); // allowed
   // tankUnit1.attack = 3;
   
   let tankUnit2 = new Unit("John");
-  tankUnit1.attackOther(tankUnit2);
+  // tankUnit1.attackOther(tankUnit2);
   
+  console.log();
+  // new unit type - Knight, inherits Unit properties
+  class Knight extends Unit {  
+    _armor = 1; // every Knight
+    
+    constructor(name) {
+      super(name); // parent constructor
+      this._health = 15; 
+      this._attack = 12;
+    } 
+    
+    get armor() {
+      return this._armor;
+    }
+    set armor(armorValue) {
+      if (armorValue < 1) {
+        throw new Error ("armor can't be less than 1");
+      }
+      this._armor = armorValue;
+    }
+    
+    attackOther(otherUnit) {
+      otherUnit.health += otherUnit.armor;
+      super.attackOther(otherUnit);
+    }
+  }
   
+  let knight1 = new Knight ("Mike");
+  console.log(knight1 instanceof Knight);
