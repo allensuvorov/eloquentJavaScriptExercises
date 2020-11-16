a92cc14865587bdc9e93baf69d06d555d7cf13fa
Allen <allenvs@mail.ru>
2020-11-16 13:10:37 +0300

    add getter code sample

--- chapter6/codeBites.js
+++ chapter6/codeBites.js
@@ -187,4 +187,10 @@ class Matrix {
 
 //#endregion
 //#region 6.11. Getters, setter, and statics
+let varyingSize =  {
+    get size() {
+        return Math.floor(Math.random()*100);
+    }
+};
+console.log(varyingSize.size);
 //#endregion
