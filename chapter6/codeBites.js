8eaffc19b86d094f9e9b80152b84e741188dd81b
Allen <allenvs@mail.ru>
2020-11-18 09:05:34 +0300

    add get and set in fahrenheit

--- chapter6/codeBites.js
+++ chapter6/codeBites.js
@@ -198,5 +198,15 @@ class Temperature {
     constructor(celsius) {
         this.celsius = celsius; // all objects will have this parameter
     }
+    get fahrenheit() {
+        return this.celsius * 1.8 + 32; // returns temperature in fahrenheit
+    }
+    set fahrenheit(value) {
+        this.celsius = (value - 32) / 1.8; // allows set temperature in fahrenheit (saved in celsius)
+    }
 };
+
+let temp = new Temperature(22);
+console.log(temp.fahrenheit);
+
 //#endregion
