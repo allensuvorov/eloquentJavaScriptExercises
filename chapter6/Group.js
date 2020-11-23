0b8a948bdd050cb8ddbb535ad5f378543cfefb9e
Allen <allenvs@mail.ru>
2020-11-23 12:21:28 +0300

    started doing Group (Set) class

---
+++ chapter6/Group.js
@@ -0,0 +1,18 @@
+class Group {
+    // Your code here.
+    constructor(){}
+    set(){}
+    add(){}
+    delete(){}
+    static from(){}
+}
+
+let group = Group.from([10, 20]);
+console.log(group.has(10));
+// → true
+console.log(group.has(30));
+// → false
+group.add(10);
+group.delete(10);
+console.log(group.has(10));
+// → false
