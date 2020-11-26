294cca2d4a7baa6db1c2ba8c79a6282b20e8aac4
Allen <allenvs@mail.ru>
2020-11-26 11:45:24 +0300

    added hint from the book, need to review

--- chapter6/IterableGroup.js
+++ chapter6/IterableGroup.js
@@ -26,3 +26,7 @@ for (let value of Group.from(["a", "b", "c"])) {
   // → a
   // → b
   // → c
+
+/* HINT: It is probably worthwhile to define a new class GroupIterator. Iterator instances should have a property that tracks the current position in the group. Every time next is called, it checks whether it is done and, if not, moves past the current value and returns it.
+
+The Group class itself gets a method named by Symbol.iterator that, when called, returns a new instance of the iterator class for that group.*/
