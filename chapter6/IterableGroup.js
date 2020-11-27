3f6d3e876decb0dbf8659de62a635381114f01f4
Allen <allenvs@mail.ru>
2020-11-27 12:41:14 +0300

    clarified - need to make the class iterable, no the array within it, the array is iterable type as is

--- chapter6/IterableGroup.js
+++ chapter6/IterableGroup.js
@@ -20,13 +20,21 @@ class Group {
     }
 }
 
-for (let value of Group.from(["a", "b", "c"])) {
+for (let value of Group.from(["a", "b", "c"]).members) {
     console.log(value);
   }
   // → a
   // → b
   // → c
 
-/* HINT: It is probably worthwhile to define a new class GroupIterator. Iterator instances should have a property that tracks the current position in the group. Every time next is called, it checks whether it is done and, if not, moves past the current value and returns it.
+/* 
+
+Matrix.prototype[Symbol.iterator] = function() {
+  return new MatrixIterator(this);
+};
+
+TASK: If you used an array to represent the group’s members, don’t just return the iterator created by calling the Symbol.iterator method on the array. That would work, but it defeats the purpose of this exercise.
+
+HINT: It is probably worthwhile to define a new class GroupIterator. Iterator instances should have a property that tracks the current position in the group. Every time next is called, it checks whether it is done and, if not, moves past the current value and returns it.
 
 The Group class itself gets a method named by Symbol.iterator that, when called, returns a new instance of the iterator class for that group.*/
