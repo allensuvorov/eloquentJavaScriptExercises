d6ef05f4ff211119df3ff49f6306aec8f244c32b
Allen <allenvs@mail.ru>
2020-12-02 11:52:46 +0300

    solved borrowing method via function.call(this, argument)

--- chapter6/hasOwnProperty.js
+++ chapter6/hasOwnProperty.js
@@ -5,7 +5,8 @@
 let map = {one: true, two: true, hasOwnProperty: true};
 
 // Fix this call
-console.log(map.hasOwnProperty("one"));
+// console.log(map.hasOwnProperty("one"));
+console.log(hasOwnProperty.call(map, "one"));
 // → true
 
 // HINT:
