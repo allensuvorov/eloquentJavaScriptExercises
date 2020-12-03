8f6d4850501a0143be6538472fd456343875c890
Allen <allenvs@mail.ru>
2020-12-03 06:33:30 +0300

    started chapter7 project: Robot

---
+++ chapter7/codeBites.js
@@ -0,0 +1,1 @@
+// Our project in this chapter is to build an automaton, a little program that performs a task in a virtual world. Our automaton will be a mail-delivery robot picking up and dropping off parcels.
