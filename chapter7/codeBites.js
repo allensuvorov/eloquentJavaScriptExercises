4c9cc78b59296878372a2f7cb0a2acbee841bfe1
Allen <allenvs@mail.ru>
2020-12-27 23:22:53 +0300

    compareRobots: will it need a new state for each of the 100 task?

--- chapter7/codeBites.js
+++ chapter7/codeBites.js
@@ -176,12 +176,11 @@ runRobot(VillageState.random(), goalOrientedRobot, []);
 
 //#region Exercise: Measuring a robot
 
-Measuring a robot
+// Write a function "compareRobots" that takes two robots (and their starting memory). It should generate 100 tasks and let each of the robots solve each of these tasks. When done, it should output the average number of steps each robot took per task.
 
-// It’s hard to objectively compare robots by just letting them solve a few scenarios. Maybe one robot just happened to get easier tasks or the kind of tasks that it is good at, whereas the other didn’t.
-
-function compareRobots(robot1, memory1, robot2, memory2) {
-
+function compareRobots(robot1, memory1, robot2, memory2)    
+{
+// will it generate a random state for each task or use one same state for all 100 tasks? 
 }
 
 compareRobots(routeRobot, [], goalOrientedRobot, []);
